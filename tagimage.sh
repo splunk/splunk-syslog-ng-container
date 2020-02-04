fe09a2a51d1ac35f92bc94d8c2163d4eed4066aa
rfaircloth-splunk <rfaircloth@splunk.com>
2020-02-04 09:05:34 -0500

    Update tagimage.sh

--- tagimage.sh
+++ tagimage.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 regex='\((.*)\)'
-sv=$(docker run --rm docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:$@ --version | head -n 2 | tail -n 1 )
+IMAGE=$@
+sv=$(docker run --rm docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:${IMAGE} --version | head -n 2 | tail -n 1 )
 [[ sv =~ $regex ]]
-docker tag docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:$@ \
+docker tag docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:${IMAGE} \
           docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:${BASH_REMATCH[1]}
 docker push docker.pkg.github.com/splunk/splunk-syslog-ng-container/splunk-syslog-ng-container:${BASH_REMATCH[1]}
